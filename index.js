5a48b6e66a545edf92678f4072bc37e35d3ee3c2
Marcushadow <arachnoshroud@gmail.com>
2021-03-17 16:30:20 +0800

    Annotations for certain functions

--- index.js
+++ index.js
@@ -3,3 +3,14 @@
     
   });
 })
+// Potentially a function to disable the opposite team's timer start button if the other timer is running (Low importance)
+
+// Function to pause the timer when a click is made (to edit the time) (minor feature unimportant)
+
+// Chess clock timer function: 
+
+// Switch button to switch to the opposite team
+
+// Pause button to pause the timer on either team
+
+// Resume button to start the timer again 
