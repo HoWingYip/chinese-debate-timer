1bd8f4156eb0902b392434dbdda238b53ada70fc
Marcus Wee <arachnoshroud@gmail.com>
2021-03-30 07:41:04 +0800

    Merge branch 'master' of https://github.com/HoWingYip/chinese-debate-timer

--- timer.js
+++ timer.js
@@ -113,26 +113,128 @@ const addTimerEventListeners = (timerElem) => {
   }
 };
 
-for (const timerElem of document.getElementsByClassName("timer")) {
-  addTimerEventListeners(timerElem);
+
+// Adds only to normal Timer set
+for(const normalTimer of document.getElementsByClassName("normal-timer")){
+  for (const timerElem of normalTimer.getElementsByClassName("timer")) {
+    addTimerEventListeners(timerElem);
+  }
 }
 
+
 // Chess clock section
-const chessTimer = document.getElementsByClassName("chess-timer")[0];
-const buttonPanel = chessTimer.getElementsByClassName("button-panel")[0];
-const swapButton = buttonPanel.getElementsByClassName("swap")[0];
-const pauseButton = buttonPanel.getElementsByClassName("chess-clock-pause")[0];
 
-swapButton.addEventListener("click", () => {
-  console.log("swap at " + chessTimer);
-  timerContainer = chessTimer.getElementsByClassName("chess-timer-internal-container")[0];
-  timerLeft = timerContainer.querySelectorAll(".timer")[0];
-  timerRight = timerContainer.querySelectorAll(".timer")[1];
 
-  if (timerLeft.dataset.running === "true" && timerRight.dataset.running === "true") {
-    console.log("Both are running");
-  } else {
-    console.log("Either one or none is running");
+
+// swapButton.addEventListener("click", () => {
+//   console.log("swap at " + chessTimer);
+//   timerContainer = chessTimer.getElementsByClassName("chess-timer-internal-container")[0];
+//   timerLeft = timerContainer.querySelectorAll(".timer")[0];
+//   timerRight = timerContainer.querySelectorAll(".timer")[1];
+
+//   if (timerLeft.dataset.running === "true" && timerRight.dataset.running === "true") {
+//     console.log("Both are running");
+//   } else {
+//     console.log("Either one or none is running");
+//   }
+// });
+
+const addChessTimerEventListeners = (chessTimerElem) => {
+
+  let IntervalStorage = {}
+  let prevTime = null;
+  let countdownIntervalId;
+
+  const timerContainer = chessTimerElem.getElementsByClassName("chess-timer-internal-container")[0];
+  const leftTimer = timerContainer.getElementsByClassName("timer")[0];
+  const rightTimer = timerContainer.getElementsByClassName("timer")[1];
+  const leftTimerButton = leftTimer.getElementsByClassName("start-stop-timer")[0]
+  const rightTimerButton = rightTimer.getElementsByClassName("start-stop-timer")[0]
+  const buttonPanel = chessTimerElem.getElementsByClassName("button-panel")[0];
+  const swapButton = buttonPanel.getElementsByClassName("swap")[0];
+  const pauseButton = buttonPanel.getElementsByClassName("chess-clock-pause")[0];
+
+  // Starting the initial timings
+  const startClock = (button) => {
+
+    timerElem = button.srcElement.closest(".timer");
+
+    const otherTimerElem = Array.from(timerContainer.getElementsByClassName("timer")).filter((elem) =>  elem != timerElem)[0]
+
+    if(otherTimerElem.dataset.running === "true" || timerElem.dataset.running === "true"){
+      stopTimer(otherTimerElem, countdownIntervalId);
+      stopTimer(timerElem, countdownIntervalId);
+    }
+    else{
+      prevTime = new Date().getTime();
+      const countdown = () => {
+        const msElapsed = BigInt(new Date().getTime() - prevTime);
+        const newTimeSeconds = getCurrentTimerDurationSeconds(timerElem) - msElapsed / 1000n;
+        updateUiAccordingToTimerState(timerElem, newTimeSeconds, countdownIntervalId);
+        if (msElapsed >= 1000n) {
+          prevTime = new Date().getTime();
+        }
+      };
+
+      countdown();
+      countdownIntervalId = setInterval(countdown, 1);
+      // OH NO MY QUEEN
+      side = 
+      console.log(side);
+      IntervalStorage[side] = countdownIntervalId;
+      timerElem.dataset.running="true";
+
+    }
+    updateUiAccordingToTimerState(timerElem, getCurrentTimerDurationSeconds(timerElem), countdownIntervalId);
   }
 
-});
+  leftTimerButton.addEventListener("click", startClock);
+  rightTimerButton.addEventListener("click", startClock);
+
+  // Implementing swapping mechanics
+  swapButton.addEventListener("click", () => {
+    if(leftTimer.dataset.running === "true" && rightTimer.dataset.running === "false"){
+      console.log("Left");
+      // console.log(IntervalStorage["left"])
+    }
+    else if(leftTimer.dataset.running === "false" && rightTimer.dataset.running === "true"){
+      console.log("right")
+    }
+  })
+
+
+
+
+
+
+  // Wrapping code
+  const leftTimerWrapper = leftTimer.getElementsByClassName("timer-input-wrapper")[0];
+  leftTimerWrapper.addEventListener("focusout", () => {
+    updateUiAccordingToTimerState(leftTimer, getCurrentTimerDurationSeconds(leftTimer), 0);
+  });
+
+  const rightTimerWrapper = rightTimer.getElementsByClassName("timer-input-wrapper")[0];
+  rightTimerWrapper.addEventListener("focusout", () => {
+    updateUiAccordingToTimerState(rightTimer, getCurrentTimerDurationSeconds(rightTimer), 0);
+  });
+  
+  for (const timerInput of getMinutesAndSecondsInputs(chessTimerElem)) {
+    timerInput.addEventListener("keypress", (event) => {
+      if (event.key < "0" || event.key > "9") {
+        event.preventDefault();
+      }
+    });
+
+    timerInput.addEventListener("input", () => {
+      timerInput.style.transition = "border-bottom-color 0.2s";
+      timerInput.style.width = `${Math.max(timerInput.value.length, 1)}ch`;
+      setTimeout(() => {
+        timerInput.style.transition = "width 0.2s, border-bottom-color 0.2s";
+      }, 0);
+    });
+  }
+};
+
+for(const chessTimerElem of document.getElementsByClassName("chess-timer")){
+  addChessTimerEventListeners(chessTimerElem);
+}
